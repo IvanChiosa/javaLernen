cc3ee19ab825856e2fe03c7219df9cf17e06bcc4
Ivan <i85chiosa@gmail.com>
2023-05-16 21:17:50 +0200

    Info

--- operation/JavaOperation.java
+++ operation/JavaOperation.java
@@ -1,8 +1,13 @@
-public static void operation() {#
 
 
-    int i = 10 + 1;
+public  class ExpressionTypeExample {
 
-    System.out.println("i7" + i7);
-    
+    public static void main(String[]args) {
+        byte b = 1;
+        short s = 1;
+        int i = 1;
+        long l = 1;
+
+        
+    }
 }
